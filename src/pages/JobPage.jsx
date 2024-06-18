a5c5f15d190a05631aa467d064a8695f7714b1e0
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-06-18 15:14:58 +0300

    cleaned up the code

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -31,10 +31,11 @@ const JobPage = () => {
             <section>
                 <div className="container m-auto py-6 px-6">
                     <Link
-                    to="/jobs"
-                    className="text-indigo-500 hover:text-indigo-600 flex items-center"
+                        to="/jobs"
+                        className="text-indigo-500 hover:text-indigo-600 flex items-center"
                     >
-                    < FaArrowLeft  className='mr-2'/> Back to Job Listings
+                        <FaArrowLeft  className='mr-2'/>
+                        Back to Job Listings
                     </Link>
                 </div>
             </section>
@@ -44,15 +45,17 @@ const JobPage = () => {
                     <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
                     <main>
                         <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-                            <div className="text-gray-500 mb-4">{ job.type }</div>
-                                <h1 className="text-3xl font-bold mb-4">{ job.title }</h1>
-                                <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                                    <FaMapMarker className='text-orange-700 mr-1' />
-                                    <p className="text-orange-700">{ job.location }</p>
-                                </div>
+                            <div className="text-gray-500 mb-4">
+                                { job.type }
                             </div>
+                            <h1 className="text-3xl font-bold mb-4">{ job.title }</h1>
+                            <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                                <FaMapMarker className='text-orange-700 mr-1' />
+                                <p className="text-orange-700">{ job.location }</p>
+                            </div>
+                        </div>
 
-                            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                        <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                             <h3 className="text-indigo-800 text-lg font-bold mb-6">Job Description</h3>
                             <p className="mb-4">{ job.description }</p>
 
