82dd1020a3042a7896af97e6242a53ca6aebf744
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-06-19 15:00:04 +0300

    add DELETE request

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,21 @@
-import { useParams, useLoaderData, Link } from 'react-router-dom'
+import { useParams, useNavigate, useLoaderData, Link } from 'react-router-dom'
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa'
 // import { useState, useEffect, React } from 'react'
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+    const navigate = useNavigate()
     const { id } = useParams()
     const job = useLoaderData()
+
+    const onDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete this listing?')
+
+        if(!confirm) return
+
+        deleteJob(jobId)
+
+        navigate('/jobs')
+    }
 
     // Data loaderin sijaan voisi käyttää myös useEffectiä, kuten JobListingsissä
     /*const [job, setJob] = useState(null)
@@ -86,7 +97,7 @@ const JobPage = () => {
                                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                                     Edit Job
                             </Link>
-                            <button
+                            <button onClick={() => onDeleteClick(job.id)}
                                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                             >
                                 Delete Job
